d7cac4fdd7d4bd35244b4d64511933a90616826d
Gabriela Marchini <gabriela.marchini@avanade.com>
2020-03-11 23:52:02 -0300

    11/03/2020

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
-  
+  description = 'Spitz';
+  url = "https://www.curtamais.com.br/uploads/conteudos/c3a9b36d2b70e47b09a984f499995952.jpg";
 
 }
