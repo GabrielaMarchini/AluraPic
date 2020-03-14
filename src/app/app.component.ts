4653ff819207b4fd22e89d65903ed48a3337aa51
Gabriela Marchini <gabriela.marchini@avanade.com>
2020-03-14 15:19:00 -0300

    14/03/2020

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   
+  photos = [
+
+  {
+    url:'https://conviteasaude.com.br/wp-content/uploads/2017/08/spitz-alemao-e1575467187871.jpg',
+    description: 'Natureza'
+  },
+
+  {
+        url: 'https://www.curtamais.com.br/uploads/conteudos/c3a9b36d2b70e47b09a984f499995952.jpg' ,
+    description:'Spitz'
+  },
+
+  
+  {    url: 'https://i.pinimg.com/originals/a7/48/18/a748184b9fb49f860fca0e49b277f833.jpg' ,
+      description:'Sol'
+
+}
+
+  ];
+   
+
+{
+
   
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PhotosModule } from './photos/photos.module';
 
 
-import { PhotoComponent } from './photos/photo/photo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PhotoComponent
+   
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PhotosModule  
   ],
   providers: [],
   bootstrap: [AppComponent]
