395054af1ac6915b2351056de1174dde7124c29a
Gabriela Marchini <gabriela.marchini@avanade.com>
2020-04-14 18:30:42 -0300

    14/04/2020

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,8 +1,6 @@
 
 import { ActivatedRoute } from '@angular/router';
-import { Component, OnInit, OnDestroy, } from '@angular/core';
-import { Subject} from 'rxjs';
-import {debounceTime} from 'rxjs/operators';
+import { Component, OnInit, } from '@angular/core';
 
 import { PhotoService } from './../photo/photo.service';
 import { Photo } from './../photo/photo';
@@ -13,11 +11,10 @@ import { Photo } from './../photo/photo';
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
-export class PhotoListComponent implements OnInit, OnDestroy {
+export class PhotoListComponent implements OnInit{
     
   photos : Photo [] = [];
   filter: string = '';
-  debounce: Subject<string> = new Subject<string>();
   hasMore: boolean = true;
   currentPage: number = 1;
   userName: string = '';
@@ -32,22 +29,16 @@ export class PhotoListComponent implements OnInit, OnDestroy {
        ngOnInit(): void {
      this.userName = this.activatedRoute.snapshot.params.userName;
      this.photos = this.activatedRoute.snapshot.data['photos'];
-     this.debounce
-     .pipe(debounceTime(300))
-     .subscribe(filter => this.filter = filter);
-       }
-
-       
-     ngOnDestroy(): void {
-      this.debounce.unsubscribe();
+     
+                
     }
-
     load(){
       this.photoService
       .listFromUserPaginated(this.userName, ++this.currentPage)
-      .subscribe(photos => 
-        { this.photos = this.photos.concat(photos);
-          if(!photos.length) this.hasMore = false;
+      .subscribe(photos => {
+        this.filter = '';
+        this.photos = this.photos.concat(photos);
+        if(!photos.length) this.hasMore = false;
 
 
     });
--- src/app/photos/photo-list/photo-list.module.ts
+++ src/app/photos/photo-list/photo-list.module.ts
@@ -1,3 +1,5 @@
+import { PhotoListComponent } from './photo-list.component';
+import { SearchComponent } from './search/search.component';
 import { CardModule } from './../../shared/components/card/card.module';
 import { FilterByDescription } from './filter-by-description.pipe';
 import { NgModule } from '@angular/core';
@@ -6,19 +8,23 @@ import { PhotosComponent } from './photos/photos.component';
 import { LoadButtonComponent } from './load-button/load-button.component';
 import { CommonModule } from '@angular/common';
 import { PhotoModule } from '../photo/photo.module';
+import { DarkenOnHoverModule } from 'src/app/shared/components/directives/darken-on-hover/darken-on-hover.module';
 
 @NgModule({
     declarations: [
-        
+        PhotoListComponent,
         PhotosComponent,
         LoadButtonComponent,
-        FilterByDescription
+        FilterByDescription,
+        SearchComponent
     ],
 
-     imports: 
-     [CommonModule, 
+     imports: [
+     
+         CommonModule, 
         PhotoModule,
-        CardModule
+        CardModule,
+        DarkenOnHoverModule
      ]
 
 })
